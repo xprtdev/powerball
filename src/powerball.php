15587812447d02c2192a2f5df00e61f06330889b
루시엘 <lusiel@lusiels-Mac-mini.local>
2021-11-15 08:34:35 +0900

    update

---
+++ src/powerball.php
@@ -0,0 +1,26 @@
+<?php
+namespace Xpertsoft\Powerball;
+
+class Powerball {
+	
+	private $start_round = '';
+	private $start_date_round = '';
+	private $start_time = '';
+
+	public function getStartRound() {
+		return $this->
+		
+	}
+
+	public function getStartDateRound() {
+		return $this->start_date_round;
+	}
+
+	public function getStartTime() {
+		return $this->start_time;
+	}
+
+	public function getCurrentRound() {
+	}
+}
+?>
